8f95b9dd3d8bf8f2bf095417d543f162949c251f
Aytac Veyisli <aytaj.veyisli@supremecourt.local>
2024-08-07 16:59:58 +0400

    first step

---
+++ src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,248 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author aytaj.veyisli
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //int x=5;
+        //System.out.println("int x="+x);
+        
+        //double d=5.5;
+     //   System.out.println("int d="+d);
+        
+      //  char c='c';
+      //  System.out.println("char c="+c);
+        
+      //  String s="Java";
+      //  System.out.println("String s="+s);
+        
+     //   boolean b=5>6;
+     //   System.out.println("boolean b="+b);
+        
+       // float f= (float) 5.5;
+     //   System.out.println("float f="+f);
+        
+       // long l=30;
+       // System.out.println("long l=30");
+        
+      //  int q=5;
+      //  int v=3;
+      //  System.out.println("c="+(q+v));
+        
+        
+        //===============================================
+       // int p=5;
+    //    int h=2;
+        
+    //    int t=p+h;
+    //    int z=p-h;
+    //    double r=p*h;
+    //    double k=(double)p/h;
+        
+    //    System.out.println("p+h="+t);
+     //   System.out.println("p-h="+z);
+      //  System.out.println("p*h="+r);
+      //  System.out.println("p/h="+k);
+        
+     //   int i=1;
+    //    int w=i++;
+     //   int m =++i;
+        
+     //   System.out.println(w);
+     //   System.out.println(m);
+        
+        
+    //    double y=47;
+    //    double x=y%10;
+        
+     //   int tam=(int)y/10;
+        
+        
+      //  System.out.println(x);
+       // System.out.println(tam);
+        
+     /*   Scanner sc  = new Scanner(System.in);  
+        
+        System.out.println("Enter a: ");
+        double a = sc.nextInt();
+       
+        System.out.println("Enter b: ");
+        double b=sc.nextInt();
+        
+        System.out.println("Choose operation: 1.+  2.-  3.*  4./");
+        //String str = sc.next();
+       // System.out.println(str.getClass());
+        int f=sc.nextInt();
+        //String str = sc.nextLine();
+        //String str = new String();
+      //  str=sc.nextLine();
+      double result=0;
+      
+        
+        if (f==1) {
+            int foo = 1;
+            result=a+b; 
+        }
+        else if(f==2){
+            result=a-b;
+        }
+        else if(f==3){
+            result=a*b;
+        }
+        else if(f==4){
+            result=a/b;
+        }
+        else{
+            result=0;
+        }
+ 
+            
+        System.out.println(result);
+      */
+        
+        
+        //loop
+        //problem1
+      /*  for (int i = 0; i < 5; i++) {
+            
+            for (int j = 0; j < 10; j++) {
+                System.out.print("*");
+            }
+            System.out.println("*");
+        }
+       */
+        //problem2
+           /* 
+            for (int i = 0; i < 10; i++) {
+                for (int j = 0; j <= i; j++) {
+                    System.out.print("*");
+                }
+                System.out.println("");
+        }
+            */
+           
+           
+           
+           
+        //problem3
+        /*
+                *
+               **
+              ***
+             ****
+            *****
+                   
+        
+            for(int i = 1; i < 8; i++) {
+                for (int j = 0; j < 7-i; j++) {
+                    System.out.print(" ");
+                }
+                for (int k = 0; k < i; k++) {
+                    System.out.print("*");
+                }
+               System.out.println("");
+            
+             }
+            
+         */   
+        
+        
+        
+        //problem4
+        /*
+              ===  ===
+              ===  ===
+              ===  ===
+              ===  ===
+        
+        int a = 0;
+          for (int i = 1; i < 5; i++) {
+             for (int m = 0; m < 2; m++) {
+                for (int j = 0; j < 3; j++) {
+                 System.out.print("=");
+                 for (int l = 0; l <=a ; l++) {
+                     System. out.print(" ");
+                 }
+                }
+                for (int j = 0; j <1; j++) {
+                 System.out.print(" ");
+                }
+              }
+            System.out.println(" ");
+          }
+        
+        
+        
+        
+        
+        
+        */
+          
+        
+     /*  Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt();
+        double d = scan.nextDouble();
+        scan.nextLine();
+        String str = scan.nextLine();
+        
+   
+        // Write your code here.
+        
+        System.out.println("String: " + str);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+     */
+                
+
+        
+       // scan.nextLine();
+        //String str = scan.nextLine();
+        
+        
+      
+        
+       
+        
+        
+ /*     Scanner input = new Scanner(System.in);
+      
+int number = input.nextInt();
+ // This line you have to add (It consumes the \n character)
+System.out.print("Text1: ");
+
+System.out.print("Text2: ");
+String text2 = input.nextLine();
+         */
+ 
+ 
+ 
+ 
+ Scanner scan = new Scanner(System.in);
+ int x = scan.nextInt();
+        if (x>99 && x<1000) {
+            System.out.println(x);
+        }
+        else{
+            System.out.println("0"+x);
+        }
+    }
+        
+         
+    }
+        
+    
+
